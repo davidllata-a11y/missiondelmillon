7f03b16bbae9ee051eb8bc09f271a5d6f4f13942
davidllata-a11y <davidllata@gmail.com>
2026-07-03 21:57:06 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementById("canvas3d");
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x111111);
+
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+);
+camera.position.set(0, 1.5, 3);
+
+const light = new THREE.HemisphereLight(0xffffff, 0x444444, 1);
+scene.add(light);
+
+const loader = new THREE.GLTFLoader();
+loader.load("models/piernas.glb", function (gltf) {
+  const model = gltf.scene;
+  model.scale.set(1, 1, 1);
+  scene.add(model);
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
